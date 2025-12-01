94592cd905ac2c16ceb00a9e1c6a634b94776c66
Marvin Zwolsman <mzwolsman@bol.com>
2025-12-01 09:51:16 +0100

    task: day 1 - part 1

--- src/solutions/day1.zig
+++ src/solutions/day1.zig
@@ -7,12 +7,30 @@ pub const solution: Solution = .{
 };
 
 fn part1(allocator: std.mem.Allocator) !void {
+    _ = allocator;
     var handle = try std.fs.cwd().openFileZ("./inputs/day1.txt", .{ .mode = .read_only });
     defer handle.close();
+    var buff: [128]u8 = undefined;
+    var in = handle.reader(&buff);
 
-    var in = handle.reader(&.{});
-    const data = try in.interface.readAlloc(allocator, try in.getSize());
-    defer allocator.free(data);
+    var dial: isize = 50;
+    var result: usize = 0;
+    while (true) {
+        const line = in.interface.takeDelimiterExclusive('\n') catch |err| {
+            switch (err) {
+                error.EndOfStream => break,
+                else => return err,
+            }
+        };
 
-    std.log.debug("read: {s}", .{data});
+        const sign: isize = if (line[0] == 'R') -1 else 1;
+        const ticks = try std.fmt.parseInt(u16, line[1..], 10);
+
+        dial = @mod(dial + (ticks * sign), 100);
+
+        if (dial == 0)
+            result += 1;
+    }
+
+    std.log.debug("{d}", .{result});
 }
